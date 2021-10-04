fed89549fbbc257deac217b5e789860c1267df68
samwel2000 <samwelgfrey@gmail.com>
2021-10-04 09:24:23 -0500

    added pinned field in project model

--- blog/models.py
+++ blog/models.py
@@ -63,6 +63,7 @@ class Project(models.Model):
     appstore_link = models.CharField(max_length=1000, null=True, blank=True)
     made_at = models.CharField(max_length=1000, default=".......")
     year = models.IntegerField(default=2021)
+    pinned = models.BooleanField(default=False)
 
     def __str__(self):
         return self.heading
